aa8379c003e2e0a084780d3d8dd093d0d5c0e812
Guillaume Gaugain <guillaume.gaugain@cloudreach.com>
2019-05-02 17:10:38 +0200

    Update Telnet function

--- TelnetPort.ps1
+++ TelnetPort.ps1
@@ -17,35 +17,43 @@ Param(
 # --------------------------------------------------------------------------------
 # Function name	: Telnet-Port
 # --------------------------------------------------------------------------------
-function global:Telnet-Port{ 
-	Param([string]$srv,[string]$port,$timeout=1000,[switch]$verbose) 
-	
-	$ErrorActionPreference = "SilentlyContinue" 
-	$tcpclient = new-Object system.Net.Sockets.TcpClient 
-	$iar = $tcpclient.BeginConnect($srv,$port,$null,$null) 
-	$wait = $iar.AsyncWaitHandle.WaitOne($timeout,$false) 
-	
-	if(!$wait)
-	{ 
-		$tcpclient.Close() 
-		if($verbose){Write-Host "Connection Timeout"} 
-		Return $false 
-	} 
-	else 
-	{ 
-		$error.Clear() 
-		$tcpclient.EndConnect($iar) | out-Null 
-		if($error[0]){if($verbose){write-host $error[0]};$failed = $true} 
-		$tcpclient.Close() 
-	} 
-	if($failed){return $false}else{return $true} 
+function global:Test-Port {
+    [CmdletBinding()]
+    Param (
+        [string] $ComputerName,
+        [int] $Port,
+        [int] $Delay = 1,
+        [int] $Count = 3
+    )
+    function Test-TcpClient ($IPAddress, $Port) {
+        $TcpClient = New-Object Net.Sockets.TcpClient
+        Try { $TcpClient.Connect($IPAddress, $Port) } Catch {}
+        If ($TcpClient.Connected) { $TcpClient.Close(); Return $True }
+        Return $False
+    }
+    function Invoke-Test ($ComputerName, $Port) {
+        Try   { [array]$IPAddress = [System.Net.Dns]::GetHostAddresses($ComputerName) | Select-Object -Expand IPAddressToString } 
+        Catch { Return $False }
+        [array]$Results = $IPAddress | % { Test-TcpClient -IPAddress $_ -Port $Port }
+        If ($Results -contains $True) { Return $True } Else { Return $False }
+    }
+    for ($i = 1; ((Invoke-Test -ComputerName $ComputerName -Port $Port) -ne $True); $i++)
+    {
+        if ($i -ge $Count) {
+            Write-Warning "Timed out while waiting for port $Port to be open on $ComputerName!"
+            Return $false
+        }
+        Write-Warning "Port $Port not open, retrying..."
+        Sleep $Delay
+    }
+    Return $true
 }
 
 # --------------------------------------------------------------------------------
 # Function name	: Main program
 # --------------------------------------------------------------------------------
 Try {
-	Telnet-Port $target $TcpPort
+	Test-Port $target $TcpPort
 }
 Catch {
   Write-Host "An error occured, test aborted : $_" -fore Red
